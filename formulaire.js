71c9173f335363898992d20237d45b867e196be2
pourgit2020 <67515383+pourgit2020@users.noreply.github.com>
2020-11-22 23:57:52 -0500

    Update formulaire.js

--- formulaire.js
+++ formulaire.js
@@ -5,44 +5,69 @@
     window.parent.postMessage("resize", "*");
   }
 
+function valider()                                    
+      { 
+          var name = document.forms["RegForm"]["Nom"];               
+          var email = document.forms["RegForm"]["Email"];    
+          var phone = document.forms["RegForm"]["Téléphone"];  
+          var what =  document.forms["RegForm"]["Sujet"];  
+          var password = document.forms["RegForm"]["Mot de passe"];  
+          var address = document.forms["RegForm"]["Adresse"];  
+          var comment = document.forms["RegForm"]["Commentaire"];  
 
-var username = document.getElementById("username");
-	var passw = document.getElementById("password");
-	var email1 = document.getElementById("Email");
-	var email2 = document.getElementById("ConfirmationEmail");
-	var MessageErreur = '';
-	var chargeSelect = document.getElementById("menu"); 
-   
-	if (!username.value) {
-		MessageErreur = MessageErreur+"<br>Le nom d'utilisateur est obligatoire!";
-	}
-	if(!passw.value) {
-		MessageErreur = MessageErreur+"<br> le mot de passe  est obligatoire!";
-	}
-	if(!email1.value) {
-		MessageErreur = MessageErreur+"<br> une adresse email est obligatoire!";
-	}
-	if (!email2.value) {
-		MessageErreur =  MessageErreur+"<br>une confirmation d'adresse email est obligatoire!";
-	}
-	
-	if (MessageErreur) {
-		e.preventDefault();
-		document.getElementById("MessageErreur").innerHTML = MessageErreur;
-		return true;
-	}
-
- 
-	function valider() {
-
-  if( !username.value|| !passw.value ) {
-  
-    alert("vous devez remplire la formulaire avant de l'envoyer !");
-    window.location.reload(true);
-    
-	
-	
-	 
-  
-  }
-  /*|| !email1.value || !email2.value */
+          if (name.value == "")                                  
+          { 
+              alert("Mettez votre nom."); 
+              name.focus(); 
+              return false; 
+          }    
+          if (address.value == "")                               
+          { 
+              alert("Mettez votre adresse."); 
+              address.focus(); 
+              return false; 
+          }        
+          if (email.value == "")                                   
+          { 
+              alert("Mettez une adresse email valide."); 
+              email.focus(); 
+              return false; 
+          }    
+          if (email.value.indexOf("@", 0) < 0)                 
+          { 
+             alert("Mettez une adresse email valide."); 
+              email.focus(); 
+              return false; 
+          }    
+          if (email.value.indexOf(".", 0) < 0)                 
+          { 
+              alert("Mettez une adresse email valide."); 
+              email.focus(); 
+              return false; 
+          }    
+          if (phone.value == "")                           
+          { 
+              alert("Mettez votre numéro de téléphone."); 
+              phone.focus(); 
+              return false; 
+          }    
+          if (password.value == "")                        
+          { 
+              alert("Saisissez votre mot de passe"); 
+              password.focus(); 
+              return false; 
+          }    
+          if (what.selectedIndex < 1)                  
+          { 
+              alert("Mettez votre cours."); 
+              what.focus(); 
+              return false; 
+          } 
+          if (comment.value == "")                  
+          { 
+             alert("Écrivez un commentaire."); 
+             comment.focus(); 
+             return false; 
+          } 
+          return true; 
+      }
